f62b0d62ebe42daff1eb95551088db4355d145c7
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-04-20 22:01:13 +0530

    feat: add get movie by id and delete movie APIs

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,4 +1,20 @@
 const Movie = require("../models/movie.model")
+const movieService = require("../services/movie.service")
+
+
+const errorResponseBody = {
+    err: "",
+    data: {},
+    message: "Something went wrong, cannot process the request",
+    success: false
+}
+
+const successResponseBody = {
+    error: {},
+    data: {},
+    message: "Successfully processed the request",
+    success: true
+}
 
 
 /**
@@ -29,6 +45,63 @@ const createMovie = async (req, res) => {
 }
 
 
+/**
+ * @description Delete a movie from the database by ID
+ * @returns 200 - Movie deleted successfully
+ */
+const deleteMovie = async (req, res) => {
+    try {
+        const response = await Movie.deleteOne({
+            _id: req.params.id
+        })
+
+        return res.status(200).json({
+            success: true,
+            error: {},
+            data: response,
+            message: "Movie deleted successfully"
+        })
+    }
+    catch (error) {
+        console.log(error)
+
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            data: {},
+            message: "Something went wrong"
+        })
+    }
+}
+
+
+/**
+ * @description  Get a movie from the database by ID
+ * @returns 200 - Movie fetched successfully
+ */
+const getMovie = async (req, res) => {
+    try {
+        const response = await movieService.getMovieById(req.params.id)
+
+        if (response.err) {
+            errorResponseBody.err = response.err
+            return res.status(response.code).json(errorResponseBody)
+        }
+
+        successResponseBody.data = response
+
+        return res.status(200).json({ successResponseBody })
+    }
+    catch (error) {
+        console.log(error)
+
+        return res.status(500).json({ errorResponseBody })
+    }
+}
+
+
 module.exports = {
-    createMovie
+    createMovie,
+    deleteMovie,
+    getMovie
 }
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,8 +1,20 @@
-const MovieController = require("../controllers/movie.controller")
+const movieController = require("../controllers/movie.controller")
+const movieMiddlewares = require("../middlewares/movie.middlewares")
+
 
 
 const routes = (app) => {
-    app.post("/mba/api/v1/movies", MovieController.createMovie)
+    app.post("/mba/api/v1/movies",
+        movieMiddlewares.validateMovieCreateRequest,
+        movieController.createMovie)
+
+    app.delete("/mba/api/v1/movies/:id",
+        movieController.deleteMovie
+    )
+
+    app.get("/mba/api/v1/movies/:id",
+        movieController.getMovie
+    )
 }
 
 
