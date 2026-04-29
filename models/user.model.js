b977c52d0c0b9008e9656eada5700d634189d9b5
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-04-29 13:22:46 +0530

    feat: hash user passwords before saving (bcrypt)

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
 
 
 const userSchema = new mongoose.Schema({
@@ -35,6 +36,13 @@ const userSchema = new mongoose.Schema({
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+})
 
 
 const User = mongoose.model('User', userSchema)
